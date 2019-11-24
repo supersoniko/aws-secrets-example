80b9808e3c523e37db2e057342b0f25e111e9b42
Sacha Reinert <sacha.reinert@outlook.com>
2019-11-24 15:20:22 +0100

    Testing if C/D works

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const app = express();
 const router = express.Router();
 
 router.get('/hello', (req, res) => res.send(`My dirty little secret is ${process.env.MY_SECRET}`));
+router.get('/cute', (req, res) => res.send(`My cute little secret is ${process.env.MY_SECRET}`));
 
 // The aws-serverless-express library creates a server and listens on a Unix
 app.use('/', router)
